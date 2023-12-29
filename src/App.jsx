d146d87d430d97aecb6986c98e1523c5d80d2690
Azamat <azamatkhabibullaevcto@gmail.com>
2023-12-30 00:58:13 +0500

    Add antd config provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Button, ConfigProvider, Space } from 'antd'
+import { StyleProvider } from '@ant-design/cssinjs'
+
 function App() {
-  return <>App</>
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: '#00b96b',
+          borderRadius: 2,
+
+          // Alias Token
+          colorBgContainer: '#f6ffed',
+        },
+      }}
+    >
+      <StyleProvider hashPriority="high">
+        <Space>
+          <Button type="primary">Primary</Button>
+          <Button>Default</Button>
+        </Space>
+      </StyleProvider>
+    </ConfigProvider>
+  )
 }
 
 export default App
